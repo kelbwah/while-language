ef09031fc67dc0fc8b98602ab7d63f2f49cebb16
kelbwah <kbebops@gmail.com>
2024-05-20 15:39:00 -0700

    Ur mom

--- repl/repl.go
+++ repl/repl.go
@@ -16,26 +16,39 @@ func Start(in io.Reader, out io.Writer) {
     for {
         fmt.Printf(PROMPT)
         scanned := scanner.Scan()
-
         if (!scanned) {
             return
         }
 
         line := scanner.Text()
-
         l := lexer.New(line)
         p := parser.New(l)
 
-        statements := p.ParseProgram()
+        
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
+        }
+        
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
 
-        for _, statement := range statements.Statements {
+        /*
+        for _, statement := range program.Statements {
             fmt.Println(statement) 
         }
 
-        /* -- If you want to see each individual token
+        - If you want to see each individual token
         for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
             fmt.Printf("%+v\n", tok)
         }
         */
+    }
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    for _, msg := range errors {
+        io.WriteString(out, "\t"+msg+"\n")
     }
 }
